e568cfb7d34fb5dea27c5b3300de3909902fa23a
lajmif <sy.lajmi@gmail.com>
2022-07-30 15:44:00 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import bgimage from "./components/bg.png";
+import Header from './components/Header';
+import Second from './components/Second';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+  <div className="App"> 
+            <div style={{ backgroundImage:`url(${bgimage})`,backgroundRepeat:"no-repeat",backgroundSize:"contain", height: "100vh"}}>
+
+
+    <Header />
+    <Second />
+
+   
+   
+      </div> 
+   </div>
   );
 }
 
---
+++ src/components/Second.js
@@ -0,0 +1,28 @@
+import Alert from 'react-bootstrap';
+import React, { Component } from 'react';
+
+
+
+
+
+
+export default class Second extends Component {
+  render() {
+    return (
+      <div>
+     
+     <Alert variant="success">
+  <Alert.Heading>Hey, nice to see you</Alert.Heading>
+  <h1 className='welcum'>
+  Greetings ladies and gentlemen, You have just entered my amazing react app.
+  </h1>
+  <hr />
+  <h1 className="welcumm">
+  Feel free to contact a supervisor for help. contact-us@gmail.com 
+  </h1>
+</Alert>
+    
+      </div>
+    )
+  }
+}
